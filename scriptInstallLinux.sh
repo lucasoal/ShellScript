01b4b3636a386e51ff6c2b3980d3075b12bc4140
lucaszst <lucasoalzst@gmail.com>
2021-01-28 22:29:03 -0300

    colors added on scriptInstallLinux.sh

--- scriptInstallLinux.sh
+++ scriptInstallLinux.sh
@@ -3,45 +3,70 @@
 clear
 
 # primeiros comandos
-echo "Buscando pacotes para atualizações"
-    sudo apt update;
-echo "Atualizando pacotes"
-    sudo apt upgrade;
-echo "Removendo programas desnecessarios"
-    sudo apt remove --purge libreoffice celluloid hexchat simple-scan pix rhythmbox;
-echo "Liberando espaço"
-    sudo apt clean;
-    sudo apt autoremove;
-    sudo apt autoclean;
-echo "Instalando ferramentas"
-    sudo apt install vlc gimp codeblocks audacity kdenlive kazam blender plank sublime;
+echo -e "\033[1;31m* Buscando pacotes para atualizações *\e[0m" && sleep 1;
+    sudo apt-get update && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
 
-# google chrome
-echo "Baixando Google Chrome Stable"
-    sudo gedit /etc/apt/sources.list;
-    deb http://dl.google.com/linux/deb/ stable main;
-    sudo apt-get update;
-echo "Insalando Google Chrome Stable"
-    sudo apt-get install google-chrome-stable;
+echo -e "\033[1;31m* Atualizando pacotes *\e[0m" && sleep 1;
+    sudo apt-get upgrade && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+echo -e "\033[1;31m* Removendo programas desnecessarios *\e[0m" && sleep 1;
+    sudo apt-get remove --purge libreoffice celluloid hexchat simple-scan pix rhythmbox && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+echo -e "\033[1;31m* Liberando espaço *\e[0m" && sleep 1;
+    sudo apt-get clean;
+    sudo apt-get autoremove;
+    sudo apt-get autoclean  && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+echo -e "\033[1;31m* Instalando ferramentas *\e[0m" && sleep 1;
+    sudo apt-get install vlc codeblocks kazam plank  && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+# git
+echo -e "\033[1;31m* Baixando Git *\e[0m" && sleep 1;
+    sudo apt-get install git
+    sudo add-apt-repository ppa:git-core/ppa
+    sudo apt-get update
+echo -e "\033[1;31m* Instalando Git *\e[0m" && sleep 1;
+    sudo apt-get install git && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+# build essentials
+echo -e "\033[1;31m* Instalando Build Essential *\e[0m" && sleep 1;
+    sudo apt-get install build-essential && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+    
+# oh my zsh
+echo -e "\033[1;31m* Instalando ZSH *\e[0m" && sleep 1;
+    sudo apt install zsh && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+    && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+# webapp
+echo -e "\033[1;31m* Baixando WebApp *\e[0m" && sleep 1;
+    sudo apt-get install webapp-manager && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
+
+# suporte para pacotes snap
+echo -e "\033[1;31m* Baixando suporte para Snap [snapd] *\e[0m" && sleep 1;
+    sudo rm /etc/apt/preferences.d/nosnap.pref && sudo apt install snapd && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
 
 # visual studio code
-echo "Baixando snap Visual Studio Code"
-    sudo snap install code --classic;
-    sudo snap refresh code;
+echo -e "\033[1;31m* Baixando snap Visual Studio Code *\e[0m" && sleep 1;
+    firefox https://code.visualstudio.com/docs/?dv=linux64_deb;
+echo -e "\033[1;31m* Instalar VS Code? *\e[0m" && sleep 1;
+    sudo dpkg -i home/lucas/Downloads/code_1.52.1-1608136922_amd64.deb
+echo -e "\033[1;31m* Removendo VS Code [.deb] * \033[30;1;31m" && sleep 1;
+    sudo apt-get rm /hom/lucas/Downloads/code_1.52.1-1608136922_amd64.deb && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
 
 # winehq
-echo "Habilitando arquitetura i386(32x)"
+echo -e "\033[1;31m* Baixando WineHQ *\e[0m" && sleep 1;
+echo -e "\033[1;31m* Habilitando arquitetura i386(32x) *\e[0m" && sleep 1;
     sudo dpkg --add-architecture i386;
-echo "Obtendo wine key"
+echo -e "\033[1;31m* Obtendo wine key *\e[0m" && sleep 1;
     wget -nc https://dl.winehq.org/wine-builds/winehq.key
     sudo apt-key add winehq.key;
-echo "Adicionando repositorio"
+echo -e "\033[1;31m* Adicionando repositorio *\e[0m" && sleep 1;
     sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main';
     sudo apt update;
-    sudo apt install --install-recommends winehq-stable;
+echo -e "\033[1;31m* Instalando WineHQ Stable *\e[0m" && sleep 1;
+    sudo apt install --install-recommends winehq-stable; && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
 
 # wps office
-echo "Instalando WPS Office"
+echo -e "\033[1;31m* Instalando WPS Office *\e[0m" && sleep 1;
     sudo apt-get update;
-    sudo apt-get install snapd;
-    sudo snap install wps-office-all-lang-no-internet;
+    sudo snap install wps-office-all-lang-no-internet && echo -e "\033[1;32m* Concluido *\e[0m" && sleep 1;
